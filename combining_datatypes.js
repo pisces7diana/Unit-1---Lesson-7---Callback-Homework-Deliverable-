cd8b54aaa16b1ad4db751f6bb8d0fcd6a0514aa3
pisces7diana <email7diana@gmail.com>
2023-11-27 12:15:49 -0800

    Combine objects, arrays, and functions more than one level deep

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -2,7 +2,7 @@
 
     // Combine objects, arrays, and functions
 const crayonBox = {
-    crayons : ["red", "yelow", "purple", "black"],
+    crayons : ["red", "yellow", "purple", "black"],
 }
 console.log(crayonBox.crayons[0]);
 
@@ -23,3 +23,37 @@ console.log(receipt[0].name)
 const apartmentBuilding = [ ["Diana", "Josh", "Howard", "Jessica"]
 ];
 console.log(apartmentBuilding[0][2]);
+
+// Combine objects, arrays, and functions more than one level deep
+
+function knit() {
+    return {
+        item: "scarf",
+        size : "6ft",
+    };
+};
+
+const knitIT = knit();
+
+console.log(knitIT.item);
+
+function crayonSelector() {
+    return {
+        crayons : ["red", "yellow", "purple", "black"],
+        };
+};
+
+const crayonsss = crayonSelector();
+
+console.log(crayonsss.crayons[2]);
+
+function powerButton() {
+    function options() {
+        console.log('Select a song')
+    }
+    return options;
+};
+
+const powerButtonON  = powerButton();
+
+powerButtonON();
