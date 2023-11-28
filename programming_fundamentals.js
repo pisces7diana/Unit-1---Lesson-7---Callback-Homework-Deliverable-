96a151a8b7b6f3b225f5f87b09a77aa7bbf4daf1
pisces7diana <email7diana@gmail.com>
2023-11-28 10:46:32 -0800

    Callback Homework Deliverable

--- programming_fundamentals.js
+++ programming_fundamentals.js
@@ -7,9 +7,9 @@
     // Which one is currently giving you the most struggle?
 
 // DRY (Don't repeat yourself ) - increase efficiency by avoiding repetition
-// KISS (Keep it simple, stupid) - simplicity is better than complicating the code
+// KISS (Keep it simple, stupid) - simplicity is better than complication
 // Avoid creating a YAGNI (You aren't going to need it) - cut the nonsense
-// Do the simplest thing that could possibly work - create a code that's simple and works
+// Do the simplest thing that could possibly work - write a code that's simple and works
 // Don't make me think - write a code that even a child can understand it
 // Write code for the maintainer - make it memorable
 // Single responsibility principle - each part of your code (like a class or a function) should be like a specialized tool. It should have one clear job or responsibility and do it without trying to handle too many things at once. 
@@ -18,17 +18,17 @@
 
 // Commenting Code
     // Comment each line of this code and describe what it is doing. Feel free to run this code and add console.logs to help you figure it out.
-    const f = l => {
-        let es = 0, p = 0, c = 1, n = 0
-        while (c <= l) {
-          n = c + p;
-          [c, p] = [n, c]
-          es += (c % 2 === 0) ? c : 0
+    const f = l => { // Declare a constant function 'f' with a parameter 'l'.
+        let es = 0, p = 0, c = 1, n = 0 // declare let variables: 'es' for even sum, 'p' for previous Fibonacci number, 'c' for current Fibonacci number, and 'n' for next Fibonacci number.
+        while (c <= l) { // Execute the while loop 'c' is less than or equal to the parameter 'l'.
+          n = c + p; // Calculate the next Fibonacci number by adding 'c'(urrent) and 'p'(revious) numbers
+          [c, p] = [n, c] // Swap values of 'c' and 'p' using destructuring assignment so 'n' in [n,c] is assigned to 'c' in [c,p] and 'c' in [n,c] is assigned to 'p' in [c,p]
+          es += (c % 2 === 0) ? c : 0 // Add 'c' to 'es' (even sum) if 'c' is an even number.
         }
-        return es
+        return es // Return the evenSum.
       }
       
-      console.log(f(55))
+      console.log(f(55)) // console.log the function 'f' with the argument of 55
     
 // Answer with comments:
     // In keeping with one of our programming principals (write code for the maintainer): What would have been a more semantic name for this function (hint: this is a problem we had for either lab or hw and is considered a classic ) - are there any other variable names or things that would make this code easier to read and understand?
